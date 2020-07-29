89254f96b8f1b44e57ebf09979ec07af1a2eee39
Matheus Vinícius <matheus.vinicius@imagineapps.com.br>
2020-07-29 13:53:52 -0300

    test

--- app/server.js
+++ app/server.js
@@ -8,28 +8,30 @@ const mysql  = require('mysql');
 const config = require('./config.js');
 const connection = mysql.createConnection(config);
 
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.json(process.env);
+  res.render('index', {})
+  res.end();
 });
 
-// app.use('/feedback/new', (req, res) => {
-//   connection.query(`INSERT INTO new(date) VALUES(now())`,function (error, results, fields) {
-//     if (error) throw error;
-//     res.json({ saved: true })
-//   });
-//   connection.end();
-// });
+app.use('/feedback/new', (req, res) => {
+  connection.query(`INSERT INTO new(date) VALUES(now())`,function (error, results, fields) {
+    if (error) throw error;
+    res.json({ saved: true })
+  });
+  connection.end();
+});
 
-// app.use('/feedback/start', (req, res) => {
-//   connection.query(`INSERT INTO start(date) VALUES(now())`,function (error, results, fields) {
-//     if (error) throw error;
-//     res.json({ saved: true })
-//   });
-//   connection.end();
+app.use('/feedback/start', (req, res) => {
+  connection.query(`INSERT INTO start(date) VALUES(now())`,function (error, results, fields) {
+    if (error) throw error;
+    res.json({ saved: true })
+  });
+  connection.end();
+});
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
