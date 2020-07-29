93ddc7aba9be50ad47430bf2438c952fe4a4d7fe
Matheus Vinícius <matheus.vinicius@imagineapps.com.br>
2020-07-29 14:04:03 -0300

    Fix http

--- app/server.js
+++ app/server.js
@@ -14,7 +14,7 @@ const connection = mysql.createConnection(config);
 const router = express.Router();
 router.get('/', (req, res) => { 
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<!DOCTYPE html><html> <head> <title>Atenas - Changelog</title> <style>body{margin: 0; overflow: hidden;}iframe{width: 100vw; height: 100vh; border: none; overflow: hidden;}</style> </head> <body> <iframe src="http://atenas.js.org/docs/log"> </body></html>')
+  res.write('<!DOCTYPE html><html> <head> <title>Atenas - Changelog</title> <style>body{margin: 0; overflow: hidden;}iframe{width: 100vw; height: 100vh; border: none; overflow: hidden;}</style> </head> <body> <iframe src="https://atenas.js.org/docs/log"> </body></html>')
   res.end();
 });
 
